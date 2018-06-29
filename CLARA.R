3b0e504eae327adf2916468b63545e108df25204
wparedesgt <wparedesgt@gmail.com>
2018-06-29 11:14:34 -0600

    CLARA

--- CLARA.R
+++ CLARA.R
@@ -1,1 +1,31 @@
 #Clustering Large Aplication CLARA
+library(tidyverse)
+library(cluster)
+library(factoextra)
+
+
+#Cargando la data
+
+proteinIntake <- read.csv("data/protein.csv")
+
+#nombres de fila, nombre de país y eliminar la variable País antes de normalizar
+
+rownames(proteinIntake) <- proteinIntake$Country
+
+#Eliminando las filas de los paises despues de nombrar cada linea
+
+proteinIntake$Country <- NULL
+
+#escalando los datos numericos
+
+proteinIntakeScaled <- as.data.frame(scale(proteinIntake))
+
+#Calculando clara con k = 4 y tamaño de muestra (muestras = 5) en el marco de datos # escalado y revisando los medoides:
+
+clarafit <- clara(proteinIntakeScaled, 4, samples = 5)
+
+clarafit$medoids
+
+#Fabricando el PLOT
+
+fviz_cluster(clarafit)
---
+++ validacion-cluster.R
@@ -0,0 +1,8 @@
+##Realización de validación de clúster
+
+library(factoextra)
+library(cluster)
+library(fpc)
+library(NbClust)
+
+
